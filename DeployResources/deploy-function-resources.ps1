b2c25b750735df73f67e27e7ab4c1d09b691da1a
Karishma Battina <67629745+karishma-battina@users.noreply.github.com>
2025-03-12 02:40:35 +0000

    Deploy resources

---
+++ DeployResources/deploy-function-resources.ps1
@@ -0,0 +1,3 @@
+# Script creates the following Azure resources using Azure CLI commands:
+# Resource Group, Storage Account, Function App, App Service Plan (Consumption Plan), 
+# Application Insights (and an Action Group and Smart Detector Alert Rule used with Application Insights)
